fcfcdaad3c7959c977bf16877089d9c1fc3c797f
h_yabiku <>
2024-02-04 13:41:15 +0900

    Add: エラーページ

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -4,6 +4,9 @@ export const getAllArticles = async (): Promise<Article[]> => {
   const response = await fetch(`http://localhost:3001/posts`, {
     cache: 'no-cache', // SSR になる
   });
+  if (!response.ok) {
+    throw new Error('サーバーエラーが発生しました');
+  }
   const articles = await response.json();
   return articles;
 };
