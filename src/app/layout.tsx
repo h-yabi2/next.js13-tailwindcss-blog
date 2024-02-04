787d4a662de1b7f8cd0d5b12a1a9cde9ffb6eed8
h_yabiku <>
2024-02-04 13:57:19 +0900

    Add: ローディングページ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Header from './Header';
 import Footer from './Footer';
+import { Suspense } from 'react';
+import Loading from './loading';
 
 export const metadata: Metadata = {
   title: 'Next.js v13 Blog Example with TypeScript and Tailwind CSS',
@@ -17,7 +19,7 @@ export default function RootLayout({
     <html lang="ja">
       <body className="container mx-auto bg-slate-100">
         <Header />
-        {children}
+        <Suspense fallback={<Loading />}>{children}</Suspense>
         <Footer />
       </body>
     </html>
