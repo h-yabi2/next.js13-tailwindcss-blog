5234f632f4cb5e491ae66b4b00a998a8bd9deb86
h_yabiku <>
2024-02-04 11:38:32 +0900

    Add: header

---
+++ src/app/Header.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="py-5 px-10 border-b flex justify-between items-center">
+      <div>
+        <h1 className="text-2xl font-extrabold">
+          <Link href="/">Next.js 13 Blog</Link>
+        </h1>
+      </div>
+      <div>
+        <nav className="text-sm">
+          <Link href="/articles/new" className="bg-orange-300 p-3 rounded-md">
+            記事を書く
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
