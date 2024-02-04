f1709c33554299af6eb833cf0a55060ec81678e5
h_yabiku <>
2024-02-04 13:38:56 +0900

    Modify: 取得したデータを表示

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,34 +1,47 @@
+import { Article } from '@/types';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-const ArticleList = () => {
+type ArticleListProps = {
+  articles: Article[];
+};
+
+const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
-      <article className="shadow my-4">
-        <Link href="#" className="hover:opacity-90 transition-opacity">
-          <Image
-            src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
-            alt="Article"
-            width={800}
-            height={600}
-          />
-        </Link>
-        <div className="bg-white p-6 flex flex-col justify-start">
-          <Link href="#">Technology</Link>
-          <Link href="#" className="text-2xl font-bold my-1">
-            Next.js の学習中
+      {articles.map((article) => (
+        <article key={article.id} className="shadow my-4">
+          <Link
+            href={`articles/${article.id}`}
+            className="hover:opacity-90 transition-opacity"
+          >
+            <Image
+              src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+              alt="Article"
+              width={800}
+              height={600}
+            />
           </Link>
-          <p className="text-sm pb-5">Published on 2050-01-01</p>
-          <Link href="#">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            varius enim in eros elementum tristique.
-          </Link>
-          <Link href="#" className="mt-5 text-pink-800 hover:underline">
-            Read more
-          </Link>
-        </div>
-      </article>
+          <div className="bg-white p-6 flex flex-col justify-start">
+            <Link href={`articles/${article.id}`}>Technology</Link>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-2xl font-bold my-1"
+            >
+              {article.title}
+            </Link>
+            <p className="text-sm pb-5">Published on {article.createdAt}</p>
+            <Link href={`articles/${article.id}`}>{article.content}</Link>
+            <Link
+              href={`articles/${article.id}`}
+              className="mt-5 text-pink-800 hover:underline"
+            >
+              Read more
+            </Link>
+          </div>
+        </article>
+      ))}
     </div>
   );
 };
