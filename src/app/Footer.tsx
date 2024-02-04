b830301093917fd6f5df6792f18d8cb6a0953334
h_yabiku <>
2024-02-04 11:42:39 +0900

    Add: footer

---
+++ src/app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="py-2 border-t">
+      <small>Next.js v13 learning project</small>
+    </footer>
+  );
+};
+
+export default Footer;
