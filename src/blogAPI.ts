8251030401e6d5db76f22fca08687ba7b0b74b0e
h_yabiku <>
2024-02-04 15:21:03 +0900

    Modify: 記事詳細取得用関数 getDetailArticle 作成

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,9 +1,26 @@
 import { Article } from './types';
+import { notFound } from 'next/navigation';
 
 export const getAllArticles = async (): Promise<Article[]> => {
   const response = await fetch(`http://localhost:3001/posts`, {
-    cache: 'no-cache', // SSR になる
+    cache: 'no-cache',
   });
+  if (!response.ok) {
+    throw new Error('サーバーエラーが発生しました');
+  }
+  const articles = await response.json();
+  return articles;
+};
+
+export const getDetailArticle = async (id: string): Promise<Article> => {
+  const response = await fetch(`http://localhost:3001/posts/${id}`, {
+    next: { revalidate: 60 },
+  });
+
+  if (response.status === 404) {
+    notFound();
+  }
+
   if (!response.ok) {
     throw new Error('サーバーエラーが発生しました');
   }
