30e00d1e420a1af6e91c92b1a2a1b36e870a33b0
h_yabiku <>
2024-02-04 15:30:29 +0900

    Modify: 記事詳細取得・表示

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -17,7 +17,7 @@ const ArticleList = ({ articles }: ArticleListProps) => {
             className="hover:opacity-90 transition-opacity"
           >
             <Image
-              src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+              src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
               alt="Article"
               width={800}
               height={600}
