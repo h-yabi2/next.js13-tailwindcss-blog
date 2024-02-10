cdd5d888f8ea59835bb705f451bfa0c7c746ece9
h_yabiku <>
2024-02-10 10:22:37 +0900

    Add: articles/not-found.tsx

---
+++ src/app/articles/not-found.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center mt-20">
+      <div className="p-8 rounded-lg shadow-md text-center bg-slate-200">
+        <h1 className="text-2xl font-bold mb-2">404</h1>
+        <p>ページが見つかりませんでした</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
