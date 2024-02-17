bff28cb93012bf6d1b1f82137031989e57109f18
h_yabiku <>
2024-02-18 08:37:37 +0900

    Modify: API - src/pages/api/create.ts 削除、 src/app/api/route.ts 内に新規作成記述移行

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -11,18 +11,15 @@ export async function GET(req: Request, res: NextResponse) {
   return NextResponse.json(data);
 }
 
-export async function POST(req: Request, res: Response) {
-  console.log('POST');
+export async function POST(req: Request, res: NextResponse) {
   const { id, title, content } = await req.json();
-  console.log(id, title, content);
-
   const { data, error } = await supabase
     .from('posts')
-    .insert([{ id, title, content, created_at: new Date().toISOString() }]);
+    .insert([{ id, title, content, createdAt: new Date().toISOString() }]);
 
   if (error) {
     return NextResponse.json(error);
   }
 
-  return NextResponse.json(data);
+  return NextResponse.json(data, { status: 201 });
 }
--- src/app/articles/new/page.tsx
+++ src/app/articles/new/page.tsx
@@ -18,7 +18,7 @@ const CreateBlogPage = () => {
     setLoading(true);
     // await createArticle(id, title, content);
     const API_URL = process.env.NEXT_PUBLIC_API_URL;
-    await fetch(`${API_URL}/api/create`, {
+    await fetch(`${API_URL}/api`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
