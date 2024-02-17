b2d364b3cae6001b71b07f815773913359d43f69
h_yabiku <>
2024-02-18 08:31:53 +0900

    Modify: API - src/pages/api/[id].ts -> src/app/api/[id].ts に変更

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,26 +1,7 @@
 import { supabase } from '@/utils/supabaseClient';
-import { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
 
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   if (req.method !== "GET") {
-//     return res.status(405).end();
-//   }
-
-//   const { data, error } = await supabase.from("posts").select("*");
-
-//   if (error) {
-//     return res.status(500).json({ error: error.message });
-//   }
-
-//   // return NextResponse.json(data);
-//   return res.status(200).json(data);
-// }
-
-export async function GET(req: Request, res: Response) {
+export async function GET(req: Request, res: NextResponse) {
   const { data, error } = await supabase.from('posts').select('*');
 
   if (error) {
@@ -45,47 +26,3 @@ export async function POST(req: Request, res: Response) {
 
   return NextResponse.json(data);
 }
-
-// GET specific post
-// export async function GET_POST(req: NextApiRequest, res: NextApiResponse) {
-//   const { id } = req.query;
-
-//   const { data, error } = await supabase
-//     .from("posts")
-//     .select("*")
-//     .eq("id", id)
-//     .single();
-
-//   if (error) return res.status(500).json({ error: error.message });
-//   if (!data) return res.status(404).json({ error: "Not found" });
-
-//   return NextResponse.json(data);
-
-//   // return res.status(200).json(data);
-// }
-
-// DELETE handler
-// export async function DELETE(req: NextApiRequest, res: NextApiResponse) {
-//   const { id } = req.query;
-
-//   const { error } = await supabase.from("posts").delete().eq("id", id);
-
-//   if (error) return res.status(500).json({ error: error.message });
-
-//   return res.status(200).json({ message: "Deleted successfully" });
-// }
-
-// // POST handler
-// export async function POST(req: NextApiRequest, res: NextApiResponse) {
-//   const { id, title, content } = req.body;
-
-//   const { data, error } = await supabase
-//     .from("posts")
-//     .insert([{ id, title, content, created_at: new Date().toISOString() }]);
-
-//   if (error) {
-//     return res.status(500).json({ error: error.message });
-//   }
-
-//   return res.status(200).json(data);
-// }
