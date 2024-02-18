a31368a289d4874c769b0f46be2d7dbc56ed4883
h_yabiku <>
2024-02-18 10:58:52 +0900

    Update: 削除機能実装

--- src/app/api/[id]/route.ts
+++ src/app/api/[id]/route.ts
@@ -16,15 +16,16 @@ export async function GET(req: Request, res: NextResponse) {
   return NextResponse.json(data, { status: 200 });
 }
 
-export async function POST(req: Request, res: NextResponse) {
-  const { id, title, content } = await req.json();
-  const { data, error } = await supabase
+export async function DELETE(req: Request, res: NextResponse) {
+  const id = req.url.split('/api/')[1];
+  const { error: deleteError } = await supabase
     .from('posts')
-    .insert([{ id, title, content, createdAt: new Date().toISOString() }]);
+    .delete()
+    .eq('id', id);
 
-  if (error) {
-    return NextResponse.json(error);
+  if (deleteError) {
+    return NextResponse.json(deleteError);
   }
 
-  return NextResponse.json(data, { status: 201 });
+  return NextResponse.json({ status: 200 });
 }
