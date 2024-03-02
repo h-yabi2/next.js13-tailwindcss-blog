ca1f1842d1ca241b0c1bb83b16abb4ba08573e58
h_yabiku <>
2024-03-02 10:30:07 +0900

    Revert "Update"
    This reverts commit e3ca000cb056284d7c51a7f3e7540474dacb9847.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-
 /** @type {import('next').NextConfig} */
-const dns = require("dns");
-dns.setDefaultResultOrder("ipv4first")
-
 const nextConfig = {
     images: {
         domains: ['source.unsplash.com'],
