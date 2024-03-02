042b226af3d5b30439356b660396efae0b3b3baf
h_yabiku <>
2024-03-02 11:08:39 +0900

    Add: install bufferutil utf-8-validate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default async function Home() {
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
   const res = await fetch(`${API_URL}/api/`, { cache: 'no-store' });
   const articles = await res.json();
+  console.log(articles);
 
   return (
     <div className="md:flex">
